7cade89ecd9c23ac8a9affe25423a988689d8cbd
Olexii Kohut <lexakogut@gmail.com>
2020-05-31 21:19:49 +0300

    finished CRUD model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,48 @@
 class PostsController < ApplicationController
-	def new
-	end
-
 	def index
 		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+	
+	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def create 
 		@post = Post.new(post_params)
 		
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+	   
+		if @post.update(post_params)
+		  	redirect_to @post
+		else
+		  	render 'edit'
+		end
+	end
+	
+	def destroy
+		@post = Post.find(params[:id])
+	   
+		@post.destroy
+		redirect_to posts_path
+	end
+
+private
 	def post_params
 		return params.require(:post).permit(:title, :text)
 	end
